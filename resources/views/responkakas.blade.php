4e3349405a3ce5a5ea59dda73b72764d03cb57fa
60900119008@uin-alauddin.ac.id <60900119008@uin-alaudin.ac.id>
2021-11-23 15:21:01 +0800

    ini file yang diubah

---
+++ resources/views/responkakas.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RESPON KAKAS MERGE</title>
+</head>
+<body>
+    <p>ini merupakan file respon merge </p>
+</body>
+</html>
